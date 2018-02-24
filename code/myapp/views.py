29546ae35ee5c6545b5d4f3a09a8187c6127c774
Shelley Wong <shelleywong@Shelleys-MacBook-Pro.local>
2018-02-24 11:07:11 -0800

    added a footer and organized css file

--- code/myapp/views.py
+++ code/myapp/views.py
@@ -7,12 +7,13 @@ def index(request):
     class_num = 'CINS 465'
     message = 'Hello World'
     example_list = ['one','two','three']
-    #for i in range(3):
-        #example_list+=[i+1]
+    example_list2 = []
+    for i in range(3):
+        example_list2 += [i+1]
     context = {
         'class_num':class_num,
         'message':message,
-        'example_list':example_list
+        'example_list':example_list,
+        'example_list2':example_list2
     }
     return render(request, 'index.html', context)
-        #{'class_num':class_num, 'message':message})
