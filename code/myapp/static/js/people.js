95760ec6ab2205fb5c8645b9ffb222db13c99442
Shelley Wong <shelleywong@Shelleys-MacBook-Pro.local>
2018-05-01 22:11:33 -0700

    added style to face match results, cleared radio selections after each turn, adjusted choices to fit on one line

--- code/myapp/static/js/people.js
+++ code/myapp/static/js/people.js
@@ -10,6 +10,8 @@ var app_student = new Vue({
     multiple_choice: [],
     checked_name: "",
     checked_names: [],
+    score: 0,
+    fin: false,
   },
 
   //Adapted from:
@@ -22,7 +24,6 @@ var app_student = new Vue({
     fetchStudentList: function() {
       $.get('/people/students/',function(user_dictionary) {
         this.users = user_dictionary.users;
-        // this.multiple_choice = this.users.slice(0,1);
         this.multiple_choice = this.users.slice();
         this.checked_names = this.users.slice();
         this.playgame();
@@ -31,78 +32,63 @@ var app_student = new Vue({
     },
     playgame: function(){
       console.log(this.users[this.i].username);
-      this.cur_student = this.users[this.i].username;
+      //initialize variables
       var rand = [];
+      this.cur_student = this.users[this.i].username;
+      this.score = 0;
       this.multiple_choice = [];
       this.checked_names = [];
-      rand.push(this.i);
-      // this.multiple_choice = this.users.slice(0,1);
-      // this.multiple_choice[0] = this.users[this.i];
+      this.fin = false;
+      //ensure current student is one of the multiple choice options, then add
+      //three other unique random students to the multiple choice options
       this.multiple_choice.push(this.users[this.i]);
-      while(rand.length < 4){
-          var randomnumber = Math.floor(Math.random()*this.users.length);
-          if(rand.indexOf(randomnumber) > -1) continue;
-          rand[rand.length] = randomnumber;
-          this.multiple_choice.push(this.users[randomnumber]);
-      }      // console.log(this.cur_student);
+      rand.push(this.i);
+      this.randIndex(rand);
       console.log(rand);
       console.log(this.multiple_choice);
     },
 
     nextImage: function() {
-
+      var rand = [];
+      //add user's selection to checked_names list; if correct, increase score
+      this.checked_names.push(this.checked_name);
+      if(this.checked_name == this.users[this.i].first_name){
+        this.score += 1;
+        this.curscore.push(1);
+      }
+      else {
+        this.curscore.push(0);
+      }
+      //if there are still more students in the list, set new current student
+      //and new random set of multiple choice options
       if(this.i < this.users.length-1) {
         this.i += 1;
+        this.checked_name = "";
         this.cur_student = this.users[this.i].username;
         this.shuffle(this.multiple_choice);
-        this.checked_names.push(this.checked_name);
-        //adapted from: https://stackoverflow.com/questions/2380019/generate-unique-random-numbers-between-1-and-100?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
-        var rand = [];
         this.multiple_choice = [];
         rand.push(this.i);
         this.multiple_choice.push(this.users[this.i]);
-        while(rand.length < 4){
-            var randomnumber = Math.floor(Math.random()*this.users.length);
-            if(rand.indexOf(randomnumber) > -1) continue;
-            rand[rand.length] = randomnumber;
-            this.multiple_choice.push(this.users[randomnumber]);
-        }
+        this.randIndex(rand);
         console.log(rand);
         this.shuffle(this.multiple_choice);
       }
       else {
+        this.showResults();
         console.log("finished with array");
       }
-      // this.i += 1;
-      // this.cur_student = this.users[this.i].username;
-      // this.shuffle(this.multiple_choice);
-      // //adapted from: https://stackoverflow.com/questions/2380019/generate-unique-random-numbers-between-1-and-100?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
-      // var rand = [];
-      // this.multiple_choice = [];
-      // rand.push(this.i);
-      // this.multiple_choice.push(this.users[this.i]);
-      // while(rand.length < 4){
-      //     var randomnumber = Math.floor(Math.random()*this.users.length);
-      //     if(rand.indexOf(randomnumber) > -1) continue;
-      //     rand[rand.length] = randomnumber;
-      //     this.multiple_choice.push(this.users[randomnumber]);
-      // }
-      // console.log(rand);
-      // this.shuffle(this.multiple_choice);
     },
 
-    // randIndex: function(index) {
-    //   var rand = [];
-    //   rand.push(index);
-    //   this.multiple_choice.push(this.users[index]);
-    //   while(rand.length < 4){
-    //       var randomnumber = Math.floor(Math.random()*this.users.length) + 1;
-    //       if(this.rand.indexOf(randomnumber) > -1) continue;
-    //       rand[rand.length] = randomnumber;
-    //       this.multiple_choice.push(this.users[randomnumber]);
-    //   }
-    //   console.log(this.multiple_choice);
-    // },
+    //adapted from:
+    //https://stackoverflow.com/questions/2380019/generate-unique-random-numbers-between-1-and-100?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+    randIndex: function(array) {
+      while(array.length < 4){
+        var randomnumber = Math.floor(Math.random()*this.users.length);
+        if(array.indexOf(randomnumber) > -1) continue;
+        array[array.length] = randomnumber;
+        this.multiple_choice.push(this.users[randomnumber]);
+      }
+    },
 
     //adapted from Fisher-Yates (aka Knuth) Shuffle:
     //https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
@@ -123,9 +109,9 @@ var app_student = new Vue({
       }
     },
 
-    // saveGuess: function() {
-    //   checked_names.push_back(this.checked_name);
-    // },
+    showResults: function() {
+      this.fin = true;
+    },
 
   },
 })
