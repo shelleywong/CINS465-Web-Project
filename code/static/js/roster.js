36498c4efa62e4a2e25aefb29dd548f6d41980b0
Shelley Wong <shelleywong@Shelleys-MacBook-Pro.local>
2018-05-13 10:56:05 -0700

    collect static

--- code/static/js/roster.js
+++ code/static/js/roster.js
@@ -15,22 +15,47 @@ var app_roster = new Vue({
   },
 
   // adapted from:
+  //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
   //https://stackoverflow.com/questions/42883835/sort-an-array-in-vue-js?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
   computed: {
     orderedUsers: function () {
-      if(this.selected == "First Name"){
-        // return _.orderBy(this.users, 'first_name')
-        return this.users.sort((a, b) => a.first_name.toLowerCase() > b.first_name.toLowerCase());
+      if(this.selected == "First Names A-M"){
+        return this.users.filter(
+          (word) => word.first_name.toLowerCase() < 'n').sort(
+          (a, b) => a.first_name.toLowerCase() > b.first_name.toLowerCase()
+        );
       }
-      else if(this.selected == "Last Name"){
-        // return _.orderBy(this.users, 'last_name')
-        return this.users.sort((a, b) => a.last_name.toLowerCase() > b.last_name.toLowerCase());
+      else if(this.selected == "First Names N-Z"){
+        return this.users.filter(
+          (word) => word.first_name.toLowerCase() > 'n').sort(
+          (a, b) => a.first_name.toLowerCase() > b.first_name.toLowerCase()
+        );
       }
-      else if(this.selected == "Username"){
-        // return _.orderBy(this.users, 'username')
-        return this.users.sort((a, b) => a.username.toLowerCase() > b.username.toLowerCase());
+      else if(this.selected == "Last Names A-M"){
+        return this.users.filter(
+          (word) => word.last_name.toLowerCase() < 'n').sort(
+          (a, b) => a.last_name.toLowerCase() > b.last_name.toLowerCase()
+        );
       }
-      else{
+      else if(this.selected == "Last Names N-Z"){
+        return this.users.filter(
+          (word) => word.last_name.toLowerCase() > 'n').sort(
+          (a, b) => a.last_name.toLowerCase() > b.last_name.toLowerCase()
+        );
+      }
+      else if(this.selected == "Usernames A-M"){
+        return this.users.filter(
+          (word) => word.username.toLowerCase() < 'n').sort(
+          (a, b) => a.username.toLowerCase() > b.username.toLowerCase()
+        );
+      }
+      else if(this.selected == "Usernames N-Z"){
+        return this.users.filter(
+          (word) => word.username.toLowerCase() > 'n').sort(
+          (a, b) => a.username.toLowerCase() > b.username.toLowerCase()
+        );
+      }
+      else if(this.selected == "Show All" || this.selected == ""){
         return this.users;
       }
     },
